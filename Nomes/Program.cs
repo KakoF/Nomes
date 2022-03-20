95ad02ed8c921bec88a45b8ff3d28b1cb4e1f958
Kako Ferrare <kakoferrare87@gmail.com>
2022-03-20 20:32:59 -0300

    first commit

---
+++ Nomes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Nomes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string primeiroNome = Console.ReadLine();
+                string segundoNome = Console.ReadLine();
+
+
+                Console.WriteLine($"Primeiro nome: {primeiroNome.ToUpper()}");
+                Console.WriteLine($"Segundo nome: {segundoNome.ToUpper()}");
+
+                Console.WriteLine($"Quantidade de caracteres do primeiro nome: {primeiroNome.Length}");
+                Console.WriteLine($"Quantidade de caracteres do segundo nome: {segundoNome.Length}");
+
+                Console.WriteLine($"Três primeiros caracteres do primeiro nome: {primeiroNome.Substring(0, 3)}");
+                Console.WriteLine($"Três primeiros caracteres do segundo nome: {segundoNome.Substring(0, 3)}");
+            }
+            finally
+            {
+                Console.WriteLine("Press enter to close...");
+                Console.ReadLine();
+            }
+           
+        }
+    }
+}
